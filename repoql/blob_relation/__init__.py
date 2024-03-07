e44e942ace9eb392f92be887bd08817c1052f04a
Minjae Gwon <beta.underalpha@gmail.com>
2024-03-07 19:49:51 +0900

    chore: package new constructors

--- repoql/blob_relation/__init__.py
+++ repoql/blob_relation/__init__.py
@@ -1,4 +1,19 @@
-from repoql.blob_relation.constructors import from_repository
+from repoql.blob_relation.constructors import (
+    from_blame,
+    from_blob,
+    from_hunk,
+    from_object,
+    from_objects,
+    from_repository,
+)
 from repoql.blob_relation.models import BlobRelation
 
-__all__ = ("from_repository", "BlobRelation")
+__all__ = (
+    "from_blame",
+    "from_repository",
+    "from_hunk",
+    "from_blob",
+    "from_object",
+    "from_objects",
+    "BlobRelation",
+)
